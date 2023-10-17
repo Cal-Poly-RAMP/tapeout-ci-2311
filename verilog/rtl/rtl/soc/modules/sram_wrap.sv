9076575fb41f7a9e709108360e13c6eb88fba5bb
Peter-Herrmann <herrmannp7@gmail.com>
2023-10-16 17:42:18 -0700

    whitebox sram, called with -sv

--- verilog/rtl/rtl/soc/modules/sram_wrap.sv
+++ verilog/rtl/rtl/soc/modules/sram_wrap.sv
@@ -58,51 +58,6 @@ module sram_wrap #(
         sram_d_rvalid_o <= sram_d_req_i;
 
     end
-    //     sram_d_gnt_o <= rst_ni ? sram_d_gnt: 1'b0;
-    //     sram_i_gnt_o <= rst_ni ? sram_i_gnt: 1'b0;
-    //     sram_d_rvalid_o <= rst_ni ? sram_d_rvalid: 1'b0;
-    //     sram_i_rvalid_o <= rst_ni ? sram_i_rvalid: 1'b0;
-
-    //     //default output
-    //     illegal_memory_o <= 1'b0;
-
-    //     //check address bounds
-    //     if (sram_d_addr_i > SRAM_END_ADDR || sram_d_addr_i < SRAM_BASE_ADDR) begin
-    //         illegal_memory_o <= 1'b1;
-    //     end
-    //     if (sram_i_addr_i > SRAM_END_ADDR || sram_i_addr_i < SRAM_BASE_ADDR) begin
-    //         illegal_memory_o <= 1'b1;
-    //     end
-
-    //     //for sram_d
-    //     if (sram_d_req_i && sram_d_gnt) begin
-    //         sram_d_addr <= sram_d_addr_i;
-    //         sram_d_rdata_o <= illegal_memory_o ? 32'hDEAD_BEEF: sram_d_read;
-    //         sram_d_rvalid <= sram_d_we_i ? 1'b1: 1'b0;
-    //     end
-    //     else if (sram_d_req_i)
-    //         sram_d_gnt <= 1'b0;
-    //     else begin
-    //         sram_d_gnt <= 1'b1;
-    //         sram_d_rvalid <= 1'b0;
-    //     end
-
-    //     //for sram_i
-    //     //access conflict
-    //     if (sram_d_req_i && sram_i_req_i && sram_d_we_i && sram_d_addr_i == sram_i_addr_i)
-    //         sram_i_gnt <= 1'b0;
-    //     else if (sram_i_req_i && sram_i_gnt) begin
-    //         sram_i_addr <= sram_i_addr_i;
-    //         sram_i_rdata_o <= illegal_memory_o ? 32'hDEAD_BEEF: sram_i_read;
-    //         sram_i_rvalid <= sram_i_we_i ? 1'b1: 1'b0;
-    //     end
-    //     else if (sram_i_req_i)
-    //         sram_i_gnt <= 1'b0;
-    //     else begin
-    //         sram_i_gnt <= 1'b1;
-    //         sram_i_rvalid <= 1'b0;
-    //     end
-    // end
 
     // SRAM Address Select Lines
     logic [SRAM_NUM_BLOCKS-1:0] cs_data, cs_inst, cs_data_prev, cs_inst_prev ;
