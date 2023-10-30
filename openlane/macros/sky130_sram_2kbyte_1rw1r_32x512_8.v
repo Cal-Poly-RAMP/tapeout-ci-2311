fe40ea94939bf3c9c1f4da600067dcb3c0123d57
Peter-Herrmann <herrmannp7@gmail.com>
2023-10-29 22:24:53 -0700

    blackbox attribute

--- openlane/macros/sky130_sram_2kbyte_1rw1r_32x512_8.v
+++ openlane/macros/sky130_sram_2kbyte_1rw1r_32x512_8.v
@@ -4,7 +4,7 @@
 // Write size: 8
 // SPDX-License-Identifier: Apache-2.0
 /// sta-blackbox
-(* whitebox *)
+(* blackbox *)
 module sky130_sram_2kbyte_1rw1r_32x512_8(
 `ifdef USE_POWER_PINS
     vccd1,
