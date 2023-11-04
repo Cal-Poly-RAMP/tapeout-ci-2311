29bbc67d80426aff31918b3d7cbd69197c7d7b78
Peter-Herrmann <herrmannp7@gmail.com>
2023-11-03 17:52:30 -0700

    Adding latest SRAM adapter

--- verilog/rtl/carp-lib/obi/wb_to_obi.sv
+++ verilog/rtl/carp-lib/obi/wb_to_obi.sv
@@ -7,7 +7,6 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module wb_to_obi (
     input               clk_i,
     // WishBone Master Ports
@@ -44,7 +43,7 @@ module wb_to_obi (
             if (read_outstanding && (rvalid_i && !read_accepted_a))
                 read_outstanding <= '0;
             if (!read_outstanding && read_accepted_a)
-                read_outstanding <= '0;
+                read_outstanding <= '1;
         end
     end
         
@@ -54,7 +53,7 @@ module wb_to_obi (
     end
 
     // Address Signals
-    assign req_o     = wbs_stb_i;
+    assign req_o     = wbs_stb_i && !read_outstanding;
     assign addr_o    = wbs_adr_i;
     assign we_o      = wbs_we_i;
     assign be_o      = wbs_sel_i;
@@ -63,8 +62,8 @@ module wb_to_obi (
     // Response Signals
     assign wbs_dat_o = rdata_i;
     assign wbs_ack_o = write_completed || 
-                      (read_outstanding && rvalid_i) ||
-                      (read_accepted_a && rvalid_i);
+                      (read_accepted_a && rvalid_i) ||
+                      (read_outstanding && rvalid_i);
 
     `ifdef verilator
         logic _unused;
