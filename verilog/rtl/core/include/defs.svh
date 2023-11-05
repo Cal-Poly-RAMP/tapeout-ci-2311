01e8adec8a286c097fabc5e55ccc8e2a1670436a
Peter-Herrmann <herrmannp7@gmail.com>
2023-11-05 11:29:58 -0800

    Revert "Branch condition generator remapping"
    This reverts commit aeadf6bfcaf4018178981b3b7fd97bcc8b782625.

--- verilog/rtl/core/include/defs.svh
+++ verilog/rtl/core/include/defs.svh
@@ -68,14 +68,14 @@ typedef enum logic [3:0] {
 // Branch Cond. Gen Defines //
 //////////////////////////////
 typedef enum logic [2:0] {
-    BEQ  = 3'b010,
+    BEQ  = 3'b000,
     BNE  = 3'b001,
     BLT  = 3'b100,
     BGE  = 3'b101,
     BLTU = 3'b110,
     BGEU = 3'b111,
 
-    NO_BRANCH = 3'b000
+    NO_BRANCH = 3'b010
 } branch_op_e;
 
 
